8ce7e4768f05f255d4f681a623f0db12c91aaab0
Nishchal Basyal <nischalbasyal2003@gmail.com>
2023-02-10 07:55:08 +0530

    Sql File Added

--- elms_system.sql
+++ elms_system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 03, 2023 at 12:39 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Generation Time: Feb 10, 2023 at 03:23 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,31 +33,30 @@ CREATE TABLE `courses` (
   `details` varchar(255) NOT NULL,
   `videoUrl` varchar(255) NOT NULL,
   `imageUrl` varchar(255) NOT NULL,
-  `isActive` int(11) NOT NULL DEFAULT 0,
+  `isActive` varchar(11) NOT NULL DEFAULT '0',
   `createdAt` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `courses`
 --
 
 INSERT INTO `courses` (`id`, `name`, `details`, `videoUrl`, `imageUrl`, `isActive`, `createdAt`) VALUES
-(2, 'DBMS', 'This is dbms', 'https://www.youtube.com/results?search_query=asp.net', 'images/Courses/c83dd16d-1dd6-424e-b9fc-a6caaa7a03f4.png', 0, '2023-01-12 23:00:01'),
-(3, 'Python Full Course', 'It is Python Course', 'https://www.youtube.com/watch?v=T936yTchDck', 'images/Courses/31b7be31-0345-44f6-bc07-be394ed17a33.png', 1, '2023-01-13 10:58:13'),
-(4, 'Java', 'This is Java', 'https://www.youtube.com/watch?v=T936yTchDck', 'images/Courses/f66f3f90-7af6-4f1b-a64f-2ce9813a63f5.png', 1, '2023-01-14 23:43:43');
+(2, 'DBMS', 'This is dbms', 'https://www.youtube.com/results?search_query=asp.net', 'images/Courses/c83dd16d-1dd6-424e-b9fc-a6caaa7a03f4.png', 'Available', '2023-01-12 23:00:01'),
+(3, 'Python Full Course', 'It is Python Course', 'https://www.youtube.com/watch?v=T936yTchDck', 'images/Courses/31b7be31-0345-44f6-bc07-be394ed17a33.png', 'Not Availab', '2023-01-13 10:58:13'),
+(4, 'Java', 'This is Java', 'https://www.youtube.com/watch?v=T936yTchDck', 'images/Courses/f66f3f90-7af6-4f1b-a64f-2ce9813a63f5.png', 'Available', '2023-01-14 23:43:43');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `enrolled_course`
+-- Table structure for table `enrolled_courses`
 --
 
-CREATE TABLE `enrolled_course` (
-  `course_id` int(11) NOT NULL,
-  `course_title` varchar(255) NOT NULL,
-  `course_desc` text NOT NULL,
-  `author` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `enrolled_courses` (
+  `id` int(20) NOT NULL,
+  `course_id` bigint(20) NOT NULL,
+  `student_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -68,19 +67,18 @@ CREATE TABLE `enrolled_course` (
 CREATE TABLE `news` (
   `id` int(11) NOT NULL,
   `news_title` varchar(255) NOT NULL,
-  `news_des` varchar(255) NOT NULL,
-  `news_img` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `news_des` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `news_title`, `news_des`, `news_img`) VALUES
-(1, 'Trump making ideal sloution for pollution', 'The Department of Education, Sport and Culture (DESC) has launched a consultation on a new uniform policy for the island\'s secondary schools.\r\n\r\nCurrently each sets their own rules under guidance from the department.\r\n\r\nDESC Minister Julie Edge said the r', ''),
-(2, 'Trump making ideal sloution for pollution', 'The Department of Education, Sport and Culture (DESC) has launched a consultation on a new uniform policy for the island secondary schools.Currently each sets their own rules under guidance from the department.DESC Minister Julie Edge said the review was ', ''),
-(3, 'Sports hubs plan to save regional pools backed by Tynwal', 'An investigation into whether creating new sports hubs would ensure the future of the Isle of Man\'s regional swimming pools has been backed by Tynwald.The Department of Education, Sport and Culture (DESC) had proposed controlling the struggling pools cent', ''),
-(4, 'Sports hubs plan to save regional pools backed by Tynwal', 'An investigation into whether creating new sports hubs would ensure the future of the Isle of Man regional swimming pools has been backed by Tynwald.The Department of Education, Sport and Culture  had proposed controlling the struggling pools centrally.Bu', '');
+INSERT INTO `news` (`id`, `news_title`, `news_des`) VALUES
+(1, 'Trump making ideal sloution for pollution', 'The Department of Education, Sport and Culture (DESC) has launched a consultation on a new uniform policy for the island\'s secondary schools.\r\n\r\nCurrently each sets their own rules under guidance from the department.\r\n\r\nDESC Minister Julie Edge said the r'),
+(2, 'India 2023 Budget Launch', 'The Department of Education, Sport and Culture (DESC) has launched a consultation on a new uniform policy for the island secondary schools.Currently each sets their own rules under guidance from the department.DESC Minister Julie Edge said the review was '),
+(3, 'Sports hubs plan to save regional pools backed by Tynwal', 'An investigation into whether creating new sports hubs would ensure the future of the Isle of Man\'s regional swimming pools has been backed by Tynwald.The Department of Education, Sport and Culture (DESC) had proposed controlling the struggling pools cent'),
+(4, 'The Next PM of United Kingdom', 'An investigation into whether creating new sports hubs would ensure the future of the Isle of Man regional swimming pools has been backed by Tynwald.The Department of Education, Sport and Culture  had proposed controlling the struggling pools centrally.Bu');
 
 -- --------------------------------------------------------
 
@@ -96,7 +94,7 @@ CREATE TABLE `students` (
   `st_desc` text NOT NULL,
   `st_contact` varchar(15) NOT NULL,
   `st_linkedin` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `students`
@@ -118,10 +116,12 @@ ALTER TABLE `courses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `enrolled_course`
+-- Indexes for table `enrolled_courses`
 --
-ALTER TABLE `enrolled_course`
-  ADD PRIMARY KEY (`course_id`);
+ALTER TABLE `enrolled_courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Course` (`course_id`),
+  ADD KEY `Student` (`student_id`);
 
 --
 -- Indexes for table `news`
@@ -146,10 +146,10 @@ ALTER TABLE `courses`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `enrolled_course`
+-- AUTO_INCREMENT for table `enrolled_courses`
 --
-ALTER TABLE `enrolled_course`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `enrolled_courses`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `news`
@@ -162,6 +162,17 @@ ALTER TABLE `news`
 --
 ALTER TABLE `students`
   MODIFY `st_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enrolled_courses`
+--
+ALTER TABLE `enrolled_courses`
+  ADD CONSTRAINT `Course` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Student` FOREIGN KEY (`student_id`) REFERENCES `students` (`st_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
